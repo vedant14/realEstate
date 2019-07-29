0433dbd47115d6def4b691afdc1b17f5d2a8f415
Vedant Lohbare <vedant.lohbare@gmail.com>
2019-07-29 21:33:58 +0530

    Login and Signup button on the landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
-  get 'static/home'
-  get 'static/about'
-  get 'static/contact_us'
-  get 'static/faq'
+	get 'static/home'
+	get 'about-us', to: 'static#about'
+	get 'contact-us', to: 'static#contact_us'
+	get 'faqs', to: 'static#faq'
 	root to: 'static#home'
 end
--- spec/features/static_spec.rb
+++ spec/features/static_spec.rb
@@ -5,4 +5,11 @@ describe 'navigate' do
 		visit root_path 
 		expect(page.status_code).to eq(200)
 	end	
+
+	it 'sees login if not signed in' do
+		visit root_path
+		logout(:user)
+		expect(page).to have_content(/Sign Up/)
+  	end
 end
+
